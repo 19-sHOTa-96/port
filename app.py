6a545531a2eec9256c4af66afb0bc9a02cb221c9
shota <shota@ml.com>
2022-09-17 01:31:34 +0400

    sqlalchemy issue restored

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
-
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 #adnresmobtana@gmail.com
