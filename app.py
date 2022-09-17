d840ff23d69f62b4d5735cde375accaf63853d9c
shota <shota@ml.com>
2022-09-17 16:07:28 +0400

    modification

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"# os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-print(os.environ.get("DATABASE_URI"))
+
 
 
 #adnresmobtana@gmail.com
