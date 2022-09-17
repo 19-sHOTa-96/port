bad8c39a7778dbfba6d8d44004b7d10a1e406788
shota <shota@ml.com>
2022-09-17 16:45:02 +0400

    modofication app

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
