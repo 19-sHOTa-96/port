e28c70640f4752ed45e552dc8c711a61e2126b08
19-sHOTa-96 <97273932+19-sHOTa-96@users.noreply.github.com>
2022-09-21 17:52:40 +0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from flask_mail import Mail, Message
 import os
 
 app = Flask(__name__)
-app.secret_key = 'ab3dc22ca4ab49c5820bab5c87489918'
+app.secret_key = os.environ.get("SECRET_KEY")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db" # os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
